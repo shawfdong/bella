9faa2c134902c9f0ea50ab9b30cac8434a025d87
shawfdong <sfd@lbl.gov>
2021-08-31 09:40:25 -0700

    systemd unit files

--- bella.py
+++ bella.py
@@ -7,63 +7,108 @@ import os.path
 import sys
 from shutil import make_archive
 import subprocess
+import smtplib
 
-logging.basicConfig(filename='/tmp/bella.log', level=logging.INFO, 
-        format='%(asctime)s - %(levelname)s - %(message)s')
-today = datetime.date.today()
-# yesterday = today - datetime.timedelta(days=1)
-logging.info('Today is {}'.format(today))
+def archive_to_gdrive(mount, root_dir, base_dir, gdrive):
+    # zip file will be temporarily stored in /data
+    base = '/data/' + base_dir
+    zip_file = base + '.zip'
 
-mon = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
-       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec') 
-# base_dir is something like 21_0406 (yy_mmdd)
-base_dir = '{:02d}_{:02d}{:02d}'.format(today.year - 2000, today.month, today.day)
-# root_dir is something like /netapp/Y2021/04-Apr (Yyyyy/mm-Mon)
-root_dir = '/netapp/Y{:04d}/{:02d}-{}'.format(today.year, today.month, mon[today.month-1])
-# zip file will be temporarily stored in /data
-base = '/data/' + base_dir
-zip_file = base + '.zip'
+    # src_dir is where raw data is located, something like /bella/pw/Y2021/04-Apr/21_0406
+    src_dir = mount + root_dir + '/' + base_dir
 
-# src_dir is where raw data is located, something like /netapp/Y2021/04-Apr/21_0406
-src_dir = root_dir + '/' + base_dir
+    # check if there is raw data in src_dir
+    if not os.path.isdir(src_dir):
+        logging.info('Source directory {} does not exist'.format(src_dir))
+        return
 
-# check if there is raw data for today
-if not os.path.isdir(src_dir):
-    logging.error('Source directory {} does not exist'.format(src_dir))
-    logging.error('Abort!')
-    sys.exit(1)
+    if len(os.listdir(src_dir)) == 0:
+        logging.info('There is no data in the source directory {}'.format(src_dir))
+        return
 
-if len(os.listdir(src_dir)) == 0:
-    logging.error('There is no data in the source directory {}'.format(src_dir))
-    logging.error('Abort!')
-    sys.exit(1)
+    logging.info("Archive data in " + src_dir)
+    root_dir2 = mount + root_dir
+    make_archive(base, 'zip', root_dir=root_dir2, base_dir=base_dir)
 
-# archive today's data
-logging.info("Archive today's data")
-make_archive(base, 'zip', root_dir=root_dir, base_dir=base_dir)
+    # dest_path is something like bellapw:data/Y021/04-Apr
+    dest_path = '{}/{}'.format(gdrive, root_dir)
 
-# dest_path is something like bella:data/Y021/04-Apr
-dest_path = 'bella:data/Y{:04d}/{:02d}-{}'.format(today.year, today.month, mon[today.month-1])
+    # make the path on Google Drive if it doesn't already exist
+    sub = subprocess.run(['/usr/bin/rclone', 'mkdir', dest_path])
 
-# make the path on Google Drive if it doesn't already exist
-logging.info("Make the path on Google Drive if it doesn't already exist")
-sub = subprocess.run(['/usr/bin/rclone', 'mkdir', dest_path])
+    # upload the zip file to Google Drive
+    logging.info("Upload the zip file to Google Drive at {}".format(dest_path))
+    sub = subprocess.run(['/usr/bin/rclone', 'copy', zip_file, dest_path])
+    if sub.returncode != 0:
+        logging.error("Failed to upload the zip file to Google Drive")
 
-# upload the zip file to Google Drive
-logging.info("Upload the zip file to Google Drive at {}".format(dest_path))
-sub = subprocess.run(['/usr/bin/rclone', 'copy', zip_file, dest_path])
-if sub.returncode != 0:
-    logging.info("Failed to upload the zip file to Google Drive")
-    logging.info('Abort!')
-    sys.exit(1)
+    # check if the files in the source and destination match
+    logging.info("Check if the files in the source and destination match")
+    sub = subprocess.run(['/usr/bin/rclone', 'check', '--size-only', zip_file, dest_path])
+    if sub.returncode == 0:
+        logging.info("Files in the source and destination match do match!")
+        # delete the temporary zip file
+        os.remove(zip_file)
+    else:
+        logging.error("Files in the source and destination match do not match!")
 
-# check if the files in the source and destination match
-logging.info("Check if the files in the source and destination match")
-sub = subprocess.run(['/usr/bin/rclone', 'check', '--size-only', zip_file, dest_path])
-if sub.returncode == 0:
-    logging.info("Files in the source and destination match do match!")
-    # delete the temporary zip file
-    os.remove(zip_file)
-else:
-    logging.error("Files in the source and destination match do not match!")
-    sys.exit(1)
+def date_to_dirs(date=datetime.date.today):
+    """Convert a date to root_dir & base_dir"""
+    
+    mon = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec') 
+    # root_dir is something like Y2021/04-Apr (Yyyyy/mm-Mon)
+    root_dir = 'Y{:04d}/{:02d}-{}'.format(date.year, date.month, mon[date.month-1])
+    # base_dir is something like 21_0406 (yy_mmdd)
+    base_dir = '{:02d}_{:02d}{:02d}'.format(date.year - 2000, date.month, date.day)
+    return root_dir, base_dir
+
+if __name__ == '__main__':
+    log = '/tmp/bella.log'
+    logging.basicConfig(filename=log, level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    conf = [{'mount': '/bella/pw/', 
+             'dirs': ['', 'p2/', 'PWlaserData/'],
+             'gdrive': 'bellapw:data'},
+            {'mount': '/bella/htw/',
+             'dirs': ['Undulator/', 'Thomson/', 'TestStand/', 
+                      'LaserUpstairs/', 'kHzLPA/'],
+             'gdrive': 'bellahtw:data'}]
+
+    # get yesterday's date (everyday, we back up previous day's data) 
+    # date = datetime.date(2021, 8, 19)
+    date = datetime.date.today() - datetime.timedelta(days=1)
+
+    root_dir, base_dir = date_to_dirs(date)
+    for c in conf:
+        if os.path.ismount(c['mount']):
+            for d in c['dirs']: 
+                root_dir2 = d + root_dir
+                archive_to_gdrive(c['mount'], root_dir2, base_dir, c['gdrive'])
+        else:
+            logging.error('NetApp is not mounted at {}!'.format(c['mount']))
+    
+    # send out email notification
+    # sender = 'sfd@belladata-migration.lbl.gov'
+    sender = 'sfd@lbl.gov'
+    receivers = ['bella-icus@lbl.gov', 'sfd@lbl.gov']
+
+    message = """From: Shawfeng Dong <sfd@lbl.gov>
+To: bella-icus@lbl.gov
+CC: sfd@lbl.gov
+Subject: [bella] daily archive log
+
+"""
+    with open(log) as f:
+        message += f.read()
+        # print(message)
+
+    try:
+        smtp = smtplib.SMTP('localhost')
+        smtp.sendmail(sender, receivers, message)
+        print("Successfully sent email")
+    except smtplib.SMTPException:
+        print("Error: unable to send email")
+
+    os.remove(log)
+
--- bella_one_shot.py
+++ bella_one_shot.py
@@ -6,64 +6,98 @@ For example, to archive the data generated on 2021-04-06 on GDrive, run:
 bella_one_shot.py 2021 4 6
 """
 
+import logging
 import argparse
 import sys
 from shutil import make_archive
 import subprocess
 import os
 import os.path
+import datetime
 
-parser = argparse.ArgumentParser(
-        description='example: bella_one_shot.py 2021 4 6')
-parser.add_argument('y', type=int, help='year')
-parser.add_argument('m', type=int, help='month')
-parser.add_argument('d', type=int, help='day')
+def archive_to_gdrive(mount, root_dir, base_dir, gdrive):
+    # zip file will be temporarily stored in /data
+    base = '/data/' + base_dir
+    zip_file = base + '.zip'
 
-args = parser.parse_args()
-year = args.y
-month = args.m
-day = args.d
+    # src_dir is where raw data is located, something like /bella/pw/Y2021/04-Apr/21_0406
+    src_dir = mount + root_dir + '/' + base_dir
 
-if month > 12 or month < 1:
-    sys.exit("Month must be an integer between 1 and 12")
+    # check if there is raw data in src_dir
+    if not os.path.isdir(src_dir):
+        logging.info('Source directory {} does not exist'.format(src_dir))
+        return
 
-mon = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+    if len(os.listdir(src_dir)) == 0:
+        logging.info('There is no data in the source directory {}'.format(src_dir))
+        return
 
-# base_dir is something like 21_0406 (yy_mmdd)
-base_dir = '{:02d}_{:02d}{:02d}'.format(year - 2000, month, day)
-# root_dir is something like /netapp/Y2021/04-Apr (Yyyyy/mm-Mon)
-root_dir = '/netapp/Y{:04d}/{:02d}-{}'.format(year, month, mon[month-1])
-# zip file will be temporarily stored in /data
-base = '/data/' + base_dir
-zip_file = base + '.zip'
+    logging.info("Archive data in " + src_dir)
+    root_dir2 = mount + root_dir
+    make_archive(base, 'zip', root_dir=root_dir2, base_dir=base_dir)
 
-# src_dir is where raw data is located, something like /netapp/Y2021/04-Apr/21_0406
-src_dir = root_dir + '/' + base_dir
+    # dest_path is something like bellapw:data/Y021/04-Apr
+    dest_path = '{}/{}'.format(gdrive, root_dir)
 
-# check if there is raw data for that day
-if not os.path.isdir(src_dir):
-    sys.exit('Source directory {} does not exist'.format(src_dir))
+    # make the path on Google Drive if it doesn't already exist
+    sub = subprocess.run(['/usr/bin/rclone', 'mkdir', dest_path])
 
-if len(os.listdir(src_dir)) == 0:
-    sys.exit('There is no data in the source directory {}'.format(src_dir))
+    # upload the zip file to Google Drive
+    logging.info("Upload the zip file to Google Drive at {}".format(dest_path))
+    sub = subprocess.run(['/usr/bin/rclone', 'copy', zip_file, dest_path])
+    if sub.returncode != 0:
+        logging.error("Failed to upload the zip file to Google Drive")
 
-# archive that day's data
-print("Archive that day's data")
-make_archive(base, 'zip', root_dir=root_dir, base_dir=base_dir)
+    # check if the files in the source and destination match
+    logging.info("Check if the files in the source and destination match")
+    sub = subprocess.run(['/usr/bin/rclone', 'check', '--size-only', zip_file, dest_path])
+    if sub.returncode == 0:
+        logging.info("Files in the source and destination match do match!")
+        # delete the temporary zip file
+        os.remove(zip_file)
+    else:
+        logging.error("Files in the source and destination match do not match!")
 
-# dest_path is something like bella:data/Y021/04-Apr
-dest_path = 'bella:data/Y{:04d}/{:02d}-{}'.format(year, month, mon[month-1])
+def date_to_dirs(date=datetime.date.today):
+    """Convert a date to root_dir & base_dir"""
+    
+    mon = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec') 
+    # root_dir is something like Y2021/04-Apr (Yyyyy/mm-Mon)
+    root_dir = 'Y{:04d}/{:02d}-{}'.format(date.year, date.month, mon[date.month-1])
+    # base_dir is something like 21_0406 (yy_mmdd)
+    base_dir = '{:02d}_{:02d}{:02d}'.format(date.year - 2000, date.month, date.day)
+    return root_dir, base_dir
 
-# make the path on Google Drive if it doesn't already exist
-sub = subprocess.run(['/usr/bin/rclone', 'mkdir', dest_path])
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    conf = [{'mount': '/bella/pw/', 
+             'dirs': ['', 'p2/', 'PWlaserData/'],
+             'gdrive': 'bellapw:data'},
+            {'mount': '/bella/htw/',
+             'dirs': ['Undulator/', 'Thomson/', 'TestStand/', 
+                      'LaserUpstairs/', 'kHzLPA/'],
+             'gdrive': 'bellahtw:data'}]
 
-# upload the zip file to Google Drive
-print("Upload the zip file to Google Drive at {}".format(dest_path))
-sub = subprocess.run(['/usr/bin/rclone', 'copy', zip_file, dest_path])
+    parser = argparse.ArgumentParser(
+                 description='example: bella_one_shot.py 2021 4 6')
+    parser.add_argument('y', type=int, help='year')
+    parser.add_argument('m', type=int, help='month')
+    parser.add_argument('d', type=int, help='day')
 
-# check if the files in the source and destination match
-sub = subprocess.run(['/usr/bin/rclone', 'check', '--size-only', zip_file, dest_path])
+    args = parser.parse_args()
+    year = args.y
+    month = args.m
+    day = args.d
+    date = datetime.date(year, month, day)
 
-# delete the temporary zip file
-os.remove(zip_file)
+    root_dir, base_dir = date_to_dirs(date)
+    for c in conf:
+        if os.path.ismount(c['mount']):
+            for d in c['dirs']: 
+                root_dir2 = d + root_dir
+                archive_to_gdrive(c['mount'], root_dir2, base_dir, c['gdrive'])
+        else:
+            logging.error('NetApp is not mounted at {}!'.format(c['mount']))
+    
